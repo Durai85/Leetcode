26a11dc754444311095e4d2d3d46134afdc63f20
Durai85 <mduraimurugan@karunya.edu.in>
2025-11-24 14:15:02 +0530

    Leetcode : 1018

---
+++ Python/PrefixesDivBy5.py
@@ -0,0 +1,8 @@
+def prefixesDivBy5(nums):
+    result = list()
+    number = ''
+    for i in nums:
+        number += str(i)
+        result.append(True if int(number,2)%5==0 else False)
+
+    return result
