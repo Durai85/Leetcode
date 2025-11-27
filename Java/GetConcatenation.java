cfab0cb11cf5ee5a14aa5b453fd55873806d43af
Durai29 <murugu6129@gmail.com>
2025-11-27 09:57:23 +0530

    Leetcode : 1929

---
+++ Java/GetConcatenation.java
@@ -0,0 +1,18 @@
+// Leetcode : 1929
+public class GetConcatenation {
+    public static int[] getConcatenation(int[] nums) {
+        int len = nums.length;
+        int[] arr = new int[2*len];
+        for(int i=0; i<arr.length; i++){
+            arr[i] = nums[i%len];
+        }
+        return arr;
+    }
+
+    public static void main(String arg[]){
+        int arr[] = getConcatenation(new int[]{1,2,3});
+        for(int a:arr){
+            System.out.println(a);
+        }
+    }
+}
