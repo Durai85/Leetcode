34b3b12a8b1175ccf6b1efcfba6a4f0a17136e2e
Durai29 <muruga6129@gmail.com>
2025-11-28 23:24:08 +0530

    Leetcode : 344

---
+++ Java/ReverseString.java
@@ -0,0 +1,11 @@
+public class ReverseString {
+    public void reverseString(char[] s) {
+        int half = s.length / 2;
+        for(int i=0,last=s.length-1; i<half; i++,last--){
+            char temp = s[i];
+            s[i] = s[last];
+            s[last] = temp;
+        }
+    }
+}
+// Leetcode : 344
