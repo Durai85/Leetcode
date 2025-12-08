74ef0d57ceb07d9eeca24a39fdf77efc017fd1eb
Durai29 <muruga6129@gmail.com>
2025-12-08 14:55:55 +0530

    Leetcode : 162

---
+++ Java/FindPeakElement.java
@@ -0,0 +1,13 @@
+// Leetcode : 162
+public class FindPeakElement {
+    public int findPeakElement(int[] nums) {
+        if(nums.length > 1){
+            if (nums[0] > nums[1]) return 0;
+            if (nums[nums.length-1] > nums[nums.length-2]) return nums.length-1;
+        }
+        for(int i=1; i<nums.length-1; i++){
+            if((nums[i] > nums[i-1]) && (nums[i] > nums[i+1])) return i;
+        }
+        return 0;
+    }
+}
