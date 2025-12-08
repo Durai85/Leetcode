a3e2e99f989ea1926d4dc5f6f82d955607651e13
Durai29 <muruga6129@gmail.com>
2025-12-08 15:07:49 +0530

    Leetcode : 162

--- Java/FindPeakElement.java
+++ Java/FindPeakElement.java
@@ -1,12 +1,30 @@
 // Leetcode : 162
+// public class FindPeakElement {
+//     public int findPeakElement(int[] nums) {
+//         if(nums.length > 1){
+//             if (nums[0] > nums[1]) return 0;
+//             if (nums[nums.length-1] > nums[nums.length-2]) return nums.length-1;
+//         }
+//         for(int i=1; i<nums.length-1; i++){
+//             if((nums[i] > nums[i-1]) && (nums[i] > nums[i+1])) return i;
+//         }
+//         return 0;
+//     }
+// }
 public class FindPeakElement {
-    public int findPeakElement(int[] nums) {
-        if(nums.length > 1){
-            if (nums[0] > nums[1]) return 0;
-            if (nums[nums.length-1] > nums[nums.length-2]) return nums.length-1;
+    public static int findPeakElement(int[] nums){
+        int n = nums.length;
+        if(n > 1){
+            if(nums[0] > nums[1]) return 0;
+            if(nums[n-1] > nums[n-2]) return n-1;
         }
-        for(int i=1; i<nums.length-1; i++){
-            if((nums[i] > nums[i-1]) && (nums[i] > nums[i+1])) return i;
+        int left = 1;
+        int right = n-2;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if((nums[mid] > nums[mid-1]) && (nums[mid] > nums[mid+1])) return mid;
+            else if(nums[mid-1] > nums[mid]) right = mid - 1;
+            else left = mid + 1;
         }
         return 0;
     }
