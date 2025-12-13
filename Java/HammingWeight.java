9db03fe6b53f23100b7ba7ebb65f23547a0b79f0
Durai29 <muruga6129@gmail.com>
2025-12-13 18:19:47 +0530

    Leetcode : 191

---
+++ Java/HammingWeight.java
@@ -0,0 +1,11 @@
+// Leetcode : 191
+public class HammingWeight {
+    public int hammingWeight(int n) {
+        int count = 0;
+        while(n > 0){
+            count += ((n & 1) == 1) ? 1 : 0;
+            n >>= 1;
+        }
+        return count;
+    }
+}
