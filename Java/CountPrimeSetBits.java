be1be4493c4525edb4fd6bb050773c72fef7d306
Durai29 <muruga6129@gmail.com>
2025-12-14 11:00:36 +0530

    Leetcode : 762

---
+++ Java/CountPrimeSetBits.java
@@ -0,0 +1,20 @@
+// Leetcode : 762
+public class CountPrimeSetBits {
+    public int countPrimeSetBits(int left, int right) {
+        int count = 0;
+        for(int temp = left; temp <= right; temp++){
+            int j = 0;
+            j = Integer.bitCount(temp);
+            count += (isPrime(j)) ? 1 : 0;
+        }
+        return count;
+    }
+
+    private boolean isPrime(int n){
+        if(n==0 || n==1) return false;
+        for(int i = 2; i*i <= n; i++){
+            if(n % i == 0) return false;
+        }
+        return true;
+    }
+}
