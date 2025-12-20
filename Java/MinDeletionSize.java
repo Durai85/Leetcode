e66ff10bb4fba630bb2f6fbf49368d3ce0845365
Durai29 <153485078+Durai29@users.noreply.github.com>
2025-12-20 14:40:03 +0530

    Leetcode : 994

---
+++ Java/MinDeletionSize.java
@@ -0,0 +1,16 @@
+// Leetcode : 994
+class Solution {
+    public int minDeletionSize(String[] strs) {
+        int fault = 0;
+        int len = strs[0].length();
+        for(int c = 0; c < len; c++){
+            for(int r = 0; r < strs.length-1; r++){
+                if(strs[r].charAt(c) > strs[r+1].charAt(c)){
+                    fault++;
+                    break;
+                }
+            }
+        }
+        return fault;
+    }
+}
