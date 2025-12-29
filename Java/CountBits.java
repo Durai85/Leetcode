8686059916a64145bb90d867fc19c80946243a54
Durai29 <muruga6129@gmail.com>
2025-12-29 17:35:15 +0530

    Leetcode : 338

---
+++ Java/CountBits.java
@@ -0,0 +1,11 @@
+// Leetcode : 338
+public class CountBits {
+    public int[] countBits(int n) {
+        int[] arr = new int[n+1];
+        arr[0] = 0;
+        for(int i = 1; i <= n; i++){
+            arr[i] = arr[i>>1] + (i&1);
+        }
+        return arr;
+    }
+}
