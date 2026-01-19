6069115a7675abdf911cb6338926050f5d206db1
Durai29 <muruga6129@gmail.com>
2026-01-19 20:56:19 +0530

    Leetcode : 125

---
+++ Java/IsPalindrome.java
@@ -0,0 +1,24 @@
+// Leetcode : 125
+
+public class IsPalindrome {
+    public boolean isPalindrome(String s) {
+        StringBuilder str =  new StringBuilder();
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(Character.isLetterOrDigit(ch)){
+                str.append(Character.toLowerCase(ch));
+            }
+        }
+
+        int left = 0;
+        int right = str.length()-1;
+        while(left < right){
+            if(str.charAt(left) != str.charAt(right)){
+                return false;
+            }
+            left ++;
+            right --;
+        }
+        return true;
+    }
+}
