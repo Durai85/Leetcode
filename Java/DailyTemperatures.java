9c0c3b84a999512e250d57dcbbdc10578b4dcd6e
Durai29 <muruga6129@gmail.com>
2026-01-22 11:11:24 +0530

    Leetcode : 739 -> Java

---
+++ Java/DailyTemperatures.java
@@ -0,0 +1,19 @@
+// Leetcode : 739
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class DailyTemperatures {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int answer[] = new int[temperatures.length];
+        Deque<Integer> stack = new ArrayDeque<>();
+        for(int i=0; i<temperatures.length;i++){
+            while(!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]){
+                int prev = stack.pop();
+                answer[prev] = i - prev;
+            }
+            stack.push(i);
+        }
+        return answer;
+    }
+}
