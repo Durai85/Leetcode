2c5ea85a0ab8b58273bcd24711867d01afb98826
Durai29 <muruga6129@gmail.com>
2026-01-23 21:24:16 +0530

    Leetcode : 150 -> Java

---
+++ Java/EvalRPN.java
@@ -0,0 +1,41 @@
+// Leetcode : 150
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.Set;
+
+public class EvalRPN {
+    public int evalRPN(String[] tokens) {
+        Set<String> operator = new HashSet<>();
+        operator.add("+");
+        operator.add("-");
+        operator.add("*");
+        operator.add("/");
+        Deque<Integer> stack = new ArrayDeque<>();
+        for(String token : tokens){
+            if(!operator.contains(token)){
+                stack.push(Integer.parseInt(token));
+            }
+            else if(stack.size()>1){
+                int b = stack.pop();
+                int a = stack.pop();
+                switch(token){
+                    case "+" :
+                        stack.push(a + b);
+                        break;
+                    case "-":
+                        stack.push(a - b);
+                        break;
+                    case "*":
+                        stack.push(a * b);
+                        break;
+                    case "/":
+                        stack.push(a / b);
+                        break;
+                }
+            }
+        }
+        return stack.pop();
+    }
+}
