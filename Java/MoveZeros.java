fafa4f4623d7afc8a5ac471499d01f557ef6db04
Durai29 <muruga6129@gmail.com>
2026-01-29 18:53:30 +0530

    Leetcode : 283 -> Java

---
+++ Java/MoveZeros.java
@@ -0,0 +1,16 @@
+// Leetcode : 283
+
+public class MoveZeros {
+    public void moveZeroes(int[] nums) {
+        int position = 0;
+        for(int i = 0; i<nums.length; i++){
+            if(nums[i] != 0){
+                // System.out.println(Arrays.toString(nums)+" "+position+" "+i);
+                int temp = nums[position]; 
+                nums[position] = nums[i];
+                nums[i] = temp;
+                position ++;
+            }
+        }
+    }
+}
