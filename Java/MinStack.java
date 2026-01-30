8766b8250a79464d340f63aaa2df1151e83d7922
Durai29 <muruga6129@gmail.com>
2026-01-30 19:03:41 +0530

    Leetcode : 155 -> Java

---
+++ Java/MinStack.java
@@ -0,0 +1,34 @@
+// Leetcode : 155
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class MinStack {
+    private Deque<Integer> stack;
+    private Deque<Integer> minStack;
+
+    public MinStack() {
+        stack = new ArrayDeque<>();
+        minStack = new ArrayDeque<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if(minStack.isEmpty() || val <= minStack.peek()){
+            minStack.push(val);
+        }
+    }
+    
+    public void pop() {
+        int val = stack.pop();
+        if(minStack.peek()==val) minStack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
